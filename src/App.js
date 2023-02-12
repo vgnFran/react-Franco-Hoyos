bc89d84819cced7d7a5a711e60a8dca6abcf6821
vgnFran <francohoyos2112@gmail.com>
2023-02-12 19:54:43 -0300

    styles producto

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
       
         <NavBar/>
-        <ItemListContainer greeting="hola"/>
+        <ItemListContainer />
         
       </header>
 
--- src/Componentes/Item/Item.js
+++ src/Componentes/Item/Item.js
@@ -1,9 +1,29 @@
+import "./Item.css"
+import { useState } from "react"
 
 const Item = ({product}) => {
+  let oferta= 0
+
+  if(product.oferta){
+    oferta=(product.precio /2)
+    console.log(oferta)
+  }else{
+   oferta= product.precio
+  }
+ 
   return (
-    <div>
-        <h2>{product.nombre}</h2>
-        <h3>{product.precio}</h3>
+    <div className="product">
+        <div className="container-img">
+          <img src={product.imagen}/>
+        </div>
+        <p className="product-name">{product.nombre}</p>
+        <div>
+          <p className="oferta"></p>
+          <p className="product-price">$ {oferta}</p>
+        </div>
+
+        <button id={product.id}>Agregar al Carrito</button>
+
     </div>
   )
 }
