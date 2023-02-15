6ade503f0d5883e45fcd1c7a6fd171a4ae51da06
vgnFran <francohoyos2112@gmail.com>
2023-02-15 17:28:04 -0300

    listo

---
+++ src/Componentes/ItemDetail/ItemDetail.js
@@ -0,0 +1,21 @@
+import "./ItemDetail.css"
+
+const ItemDetail = ({detail}) => {
+
+   console.log(detail)
+    
+    
+  return (
+    <div className="detail-container">
+      <div className="detail">
+      <img src={detail.imagen} alt={detail.nombre} />
+      <h2> {detail.nombre} </h2>
+      <h3> $ {detail.precio} </h3>
+      <button>Agregar</button>
+      </div>
+      
+      </div>
+  )
+}
+
+export default ItemDetail
--- src/Pages/ItemDetailContainer/ItemDetailContainer.js
+++ src/Pages/ItemDetailContainer/ItemDetailContainer.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useEffect,useState } from 'react'
+import ItemDetail from "../../Componentes/ItemDetail/ItemDetail"
 
 
 const ItemDetailContainer = () => {
+
   const { id } = useParams()
   console.log(id)
-  
 
+  const [productId,SetproductId]=useState({})
+
+  useEffect(()=>{
+    const detailProduct= fetch("/data.json")
+  .then((data)=> data.json())
+  .then((data) =>{
+    if(id){
+      const filterDetails= data.find((prod)=> prod.id == id)
+      SetproductId(filterDetails)
+    }
+  })
+
+  },[id])
+
+  console.log(productId)
+  
   return (
-    <div>ItemDetailContainer</div>
+    <div><ItemDetail detail={productId}/></div>
   )
 }
 
