d8afdc3231711d0e565c5d49b5f4973d61cec9ef
vgnFran <francohoyos2112@gmail.com>
2023-02-27 00:25:52 -0300

    buttons cart

--- src/Componentes/ItemDetail/ItemDetail.js
+++ src/Componentes/ItemDetail/ItemDetail.js
@@ -1,15 +1,25 @@
 import "./ItemDetail.css"
 import ItemCount from "../ItemCount/ItemCount"
 import { useState } from "react"
+import { Cart } from "../../Pages/Cart/Cart"
+import { useNavigate } from "react-router-dom"
 
 const ItemDetail = ({detail}) => {
 
-  console.log(detail)
+  // console.log(detail)
   const [count,setCount]= useState(0)
+
   
-  const toCart= ()=>{
-    console.log(detail)
-  }
+  const toCart= (e)=>{
+    e.preventDefault()
+    if (count > 0){
+      console.log({...detail,quantity:count})      
+    }
+   
+    }
+    
+  const navigate= useNavigate()  
+
 
     
   return (
@@ -20,7 +30,12 @@ const ItemDetail = ({detail}) => {
         <h3> $ {detail.precio} </h3>
         <h4>Cantidad disponible: ({detail.cantidad})</h4>
         <ItemCount count={count} setCount={setCount} cantidad={detail.cantidad}/>
-        <button onClick={toCart}>Agregar al Carrito</button>    
+        <button onClick={toCart}>Agregar al Carrito</button>
+        <div className="buttons-fin">
+          <button onClick={()=>{navigate('/')}}>Seguir comprando</button>
+          <button onClick={()=>{navigate('/cart')}}>Finalizar Compra</button>    
+        </div>
+
       </div>
         
     </div>
